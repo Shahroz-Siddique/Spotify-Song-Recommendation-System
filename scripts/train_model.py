688deecb2e2bff717d86561e758b33b06fabcab5
shahroz_siddique <shhrzsiddique@gmail.com>
2025-05-21 14:50:50 +0500

    Traing, Evaluate, and Run Pipline

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,6 +1,7 @@
 from src.data.make_dataset import load_raw_data, clean_data, save_processed_data
 from src.features.build_features import normalize_features
 
+
 if __name__ == '__main__':
     df = load_raw_data('/home/foxtech/SHAHROZ_PROJ/Song Recommendation System/data/dataset.csv.zip')
     df = clean_data(df)
